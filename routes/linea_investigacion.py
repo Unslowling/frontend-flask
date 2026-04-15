c4df2fc04a0cc0d3fb7c9050d4e1826daf4d79c2
Alisson Estrada <alissonestrada881@gmail.com>
2026-04-15 16:59:41 -0500

    CRUD linea investigacion - crear

--- routes/linea_investigacion.py
+++ routes/linea_investigacion.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, request, redirect, url_for, flash
 from services.api_service import ApiService
 
 linea_bp = Blueprint('linea', __name__)
@@ -9,3 +9,24 @@ api = ApiService()
 def listar_lineas():
     datos = api.listar("linea_investigacion")
     return render_template("pages/linea_investigacion/lista.html", datos=datos)
+
+@linea_bp.route('/linea_investigacion/crear', methods=['GET', 'POST'])
+def crear_linea():
+    if request.method == 'POST':
+        # Capturamos los datos enviados desde el formulario HTML
+        datos_nuevos = {
+            "nombre": request.form.get("nombre"),
+            "descripcion": request.form.get("descripcion")
+        }
+        
+        # Llamamos al método crear del ApiService
+        exito, mensaje = api.crear("linea_investigacion", datos_nuevos)
+        
+        if exito:
+            flash(mensaje, "success")
+            return redirect(url_for('linea.listar_lineas'))
+        else:
+            flash(mensaje, "danger")
+            
+    # Si es una petición GET, simplemente renderizamos el formulario en blanco
+    return render_template("pages/linea_investigacion/crear.html")
