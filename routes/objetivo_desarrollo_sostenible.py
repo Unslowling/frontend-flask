d8cc40ea4774ca21cbfbaea9a628bb381bdc94a4
Alisson Estrada <alissonestrada881@gmail.com>
2026-04-15 22:36:08 -0500

    CRUD ODS - create

--- routes/objetivo_desarrollo_sostenible.py
+++ routes/objetivo_desarrollo_sostenible.py
@@ -9,3 +9,22 @@ api = ApiService()
 def listar_ods():
     datos = api.listar("objetivo_desarrollo_sostenible")
     return render_template("pages/objetivo_desarrollo_sostenible/lista.html", datos=datos)
+
+# CREAR
+@ods_bp.route('/ods/crear', methods=['GET', 'POST'])
+def crear_ods():
+    if request.method == 'POST':
+        datos_nuevos = {
+            "nombre": request.form.get("nombre"),
+            "categoria": request.form.get("categoria")
+        }
+        
+        exito, mensaje = api.crear("objetivo_desarrollo_sostenible", datos_nuevos)
+        
+        if exito:
+            flash(mensaje, "success")
+            return redirect(url_for('ods.listar_ods'))
+        else:
+            flash(mensaje, "danger")
+            
+    return render_template("pages/objetivo_desarrollo_sostenible/crear.html")
